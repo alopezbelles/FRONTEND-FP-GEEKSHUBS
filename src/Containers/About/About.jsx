8af083804be19eb87e9ae3547892bf6d452e8e6b
alopezbelles <alopezbelles@gmail.com>
2023-01-06 19:48:52 +0100

    about layout page styles

--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -2,13 +2,31 @@ import React from "react";
 import "./About.css";
 
 
+import { Col, Container, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
 
 const About = () => {
 
     return (
-        <div>
+        
+        <Container className="container-fluid aboutDesign d-flex flex-column align-content-center justify-content-center">
 
-        </div>
+            <Row className="row1Design d-flex align-content-center justify-content-center">
+                <Col className="col1Design">About Waves.
+                </Col>
+                {/* <Col className="col2Design">Discover the favorite spots for surfers 
+                </Col> */}
+            </Row>
+
+        <Container className="container-fluid cont2Design">
+            Container2
+        </Container>
+
+
+
+        </Container>
     )
 
 
