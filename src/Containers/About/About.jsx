4a608bb0b5fc2c5747957838d9ec251255f5b26e
alopezbelles <alopezbelles@gmail.com>
2023-01-07 12:04:23 +0100

    working on About page, responsive

--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -1,35 +1,35 @@
 import React from "react";
 import "./About.css";
 
-
 import { Col, Container, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Image from "react-bootstrap/Image";
+import icon1 from "../../aessets/icon1.png";
+import icon2 from "../../aessets/icon2.png";
+import icon3 from "../../aessets/icon3.png";
+import icon4 from "../../aessets/icon4.png";
 
 
 const About = () => {
+  return (
+    <Container className="container-fluid aboutDesign d-flex flex-column align-content-center justify-content-center">
+      <Row className="row1Design d-flex align-content-center justify-content-center">
+        <Col className="col1Design">About Waves.</Col>
+       
+      </Row>
 
-    return (
-        
-        <Container className="container-fluid aboutDesign d-flex flex-column align-content-center justify-content-center">
-
-            <Row className="row1Design d-flex align-content-center justify-content-center">
-                <Col className="col1Design">About Waves.
-                </Col>
-                {/* <Col className="col2Design">Discover the favorite spots for surfers 
-                </Col> */}
-            </Row>
-
-        <Container className="container-fluid cont2Design">
-            Container2
-        </Container>
-
-
-
-        </Container>
-    )
-
-
-}
+      <Row className="container-fluid row2Design d-flex  align-content-center justify-content-evenly">
+        {/* <Col className="infoBox">Col
+            </Col> */}
+        <div className="infoBox d-flex align-content-center justify-content-center">
+        <Image className="iconDesign" src={icon1}></Image></div>
+        <div className="infoBox d-flex align-content-center justify-content-center"><Image className="iconDesign" src={icon2}></Image></div>
+        <div className="infoBox d-flex align-content-center justify-content-center"><Image className="iconDesign" src={icon3}></Image></div>
+        <div className="infoBox d-flex align-content-center justify-content-center"><Image className="iconDesign" src={icon4}></Image></div>
+        {/* <div className="infoBox">hola</div> */}
+      </Row>
+    </Container>
+  );
+};
 
 export default About;
