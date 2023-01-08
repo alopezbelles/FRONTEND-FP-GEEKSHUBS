de62854a8d77e89b65073daed756d585620af38b
alopezbelles <alopezbelles@gmail.com>
2023-01-08 19:41:11 +0100

    About page responsive OK

--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -8,6 +8,9 @@ import icon1 from "../../aessets/icon1.png";
 import icon2 from "../../aessets/icon2.png";
 import icon3 from "../../aessets/icon3.png";
 import icon4 from "../../aessets/icon4.png";
+import icon5 from "../../aessets/facebook.png";
+import icon6 from "../../aessets/instagram.png";
+import icon7 from "../../aessets/twitter.png";
 
 const About = () => {
   return (
@@ -36,8 +39,21 @@ const About = () => {
 
       <Row className="row3Design d-flex align-content-center justify-content-center">
         <div className="descriptionBox">
-        Waves is an application where you will find the best locations for surfing. In addition, in each spot you will find relevant information such as weather information, type of beach, type of waves, access and the existence of lifeguards or first aid stations.  
+          <p>
+            Waves is an application where you will find the best locations for
+            surfing. In addition, in each spot you will find relevant
+            information such as weather information, type of beach, type of
+            waves, access and the existence of lifeguards or first aid stations.
+            Access the spots safely and responsibly, and don't forget to reduce
+            your footprint to a minimum.{" "}
+          </p>
         </div>
+      </Row>
+
+      <Row className="row4Design d-flex align-content-center justify-content-center">
+        <Image className="logoMedia" src={icon5}></Image>
+        <Image className="logoMedia" src={icon6}></Image>
+        <Image className="logoMedia" src={icon7}></Image>
       </Row>
     </Container>
   );
