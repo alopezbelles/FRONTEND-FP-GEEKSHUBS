16597c79933d46743b6f30f071b8200920672386
alopezbelles <alopezbelles@gmail.com>
2023-01-08 20:11:39 +0100

    navbar login OK

--- src/Components/Header2/Header2.jsx
+++ src/Components/Header2/Header2.jsx
@@ -13,7 +13,12 @@ import Image from "react-bootstrap/Image";
 function Header2() {
   const [collapsed, setCollapsed] = useState(true);
   const navigate = useNavigate();
+  const token = localStorage.getItem("jwt");
 
+  const logout = () => {
+    localStorage.removeItem("jwt");
+    navigate("/");
+  };
   useEffect(() => {
     const resizeListener = () => {
       if (window.innerWidth > 768) {
@@ -24,7 +29,55 @@ function Header2() {
     return () => window.removeEventListener("resize", resizeListener);
   }, []);
 
-  return (
+  if (token) {
+    return (
+      <Nav expand="sm" className="navbar navbar-expand-lg navbar-dark  navBar2">
+        <Navbar.Brand onClick={() => navigate("/")}>
+          <Image src={logoWaves} className="logoNavbar2"></Image>
+        </Navbar.Brand>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={() => setCollapsed(!collapsed)}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className={`navbar-collapse ${collapsed ? "collapse" : ""}`}>
+          <ul className="navbar-nav mr-auto hover">
+            <li className="nav-item active">
+              <a
+                onClick={() => navigate("/spots")}
+                className="nav-link mx-auto mt-1 mb-md-1 text-center"
+              >
+                Spots
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a
+                onClick={() => navigate("/about")}
+                className="nav-link mx-auto mt-1 mb-md-1 text-center"
+              >
+                About
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav mr-auto  linksRight hover">
+            <li className="nav-item active">
+              <a
+                onClick={() => logout()}
+                className="nav-link mx-auto mt-1 mb-md-1 text-center"
+              >
+                Logout
+              </a>
+            </li>
+            
+          </ul>
+        </div>
+      </Nav>
+    );
+  } else {
+    return (
     <Nav expand="sm" className="navbar navbar-expand-lg navbar-dark  navBar2">
       <Navbar.Brand onClick={() => navigate("/")}>
         <Image src={logoWaves} className="logoNavbar2"></Image>
@@ -76,7 +129,8 @@ function Header2() {
         </ul>
       </div>
     </Nav>
-  );
+    );
+  }
 }
 
 export default Header2;
