a5305f176d6f660ae8f3b40b350be817c619dc7e
alopezbelles <alopezbelles@gmail.com>
2023-01-13 17:10:43 +0100

    calling image path from database for spots targets

--- src/Components/SpotTarget/SpotTarget.jsx
+++ src/Components/SpotTarget/SpotTarget.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import "./SpotTarget.css";
 
 const SpotTarget = ({ spot, selectionSpot }) => {
+//   const cardBackground = {
+//     background:
+//       "url(https://xsurf.es/wp-content/uploads/2021/01/Surf-en-Espana.jpg)",
+//     backgroundSize: "cover",
+//   };
   const cardBackground = {
     background:
-      "url(https://xsurf.es/wp-content/uploads/2021/01/Surf-en-Espana.jpg)",
+      "url(https://turismolanzarote.com/wp-content/uploads/2017/03/Famara_Jeziel_12.jpg)",
     backgroundSize: "cover",
   };
   return (
     <div
-      className="targetDesign imageSpot justify-content-end"
+      className="targetDesign justify-content-end"
       style={cardBackground}
       onClick={() => selectionSpot(spot)}
     >
